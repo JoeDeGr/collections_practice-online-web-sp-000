6f5877e1dd758fede9b4d61d1c2b624e61a8e8c3
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-12 01:51:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,17 +6,17 @@ def sort_array_desc(int)
   int.sort.reverse
 end
 
-# def sort_array_char_count(array)
-#   array.sort do |a, b|
-#     a.scan(/[a-z A_Z]/).count <=> b.scan(/[a-z A_Z]/).count
-#   end
-# end
-
 def sort_array_char_count(array)
   array.sort do |a, b|
-    a.length <=> b.length
+    a.scan(/[a-z A_Z]/).count <=> b.scan(/[a-z A_Z]/).count
   end
 end
+
+# def sort_array_char_count(array)
+#   array.sort do |a, b|
+#     a.length <=> b.length
+#   end
+# end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
